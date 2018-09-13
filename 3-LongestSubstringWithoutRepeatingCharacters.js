ab19612b0508b6e2960481a6cf2cf8baf10538d4
ChenCyl <programming2@163.com>
2018-09-13 22:32:34 +0800

    滑动窗口
    有bug

--- 3-LongestSubstringWithoutRepeatingCharacters.js
+++ 3-LongestSubstringWithoutRepeatingCharacters.js
@@ -23,12 +23,39 @@ Explanation: The answer is "wke", with the length of 3.
  * @param {string} s
  * @return {number}
  */
+
+// 解法2：滑动窗口法
+//       keep track of 子串开头位置、结尾位置、长度、哈希表
+//       扫描到a的重复字符a'则将子字符串开头定位到a的下一位
 var lengthOfLongestSubstring = function(s) {
+    var i = 0, j = 0, maxlen = 0, hash = {};
+    
+    while (j <= s.length) {
+        // 如果不是重复的字符
+        // 第二个条件是因为：如abcba，b重复时i跳到c，但是a仍然在哈希表中，所以位置一定要比i大
+        if (!hash[s[j]] && hash[s[j]] >= i) {
+            hash[s[j]] = j;
+            len = j - i + 1;
+            j++;
+            maxlen = maxlen >= len ? maxlen : len;
+        }
+        else {
+            i = hash[s[j]] + 1;
+            hash[s[j]] = j;
+        }
+    }
+   return maxlen;
+}
+    
+
+// 解法1 ： 从头（i）开始扫描，将扫描到的字符放进一个对象里，遇到重复的字符则清空字符串，i++，重复上述步骤。
+// 效率低
+var lengthOfLongestSubstring2 = function(s) {
     var obj = {};
     var maxlen = 0;
-    for (let i = 0; i < s.length; i++) {
+    for (let i = 0; i < s.length; i++) { // i：子字符串起始位置
         var len = 0;
-        for (let j = i; j < s.length; j++) {
+        for (let j = i; j < s.length; j++) { 
             // !!!一定要注意这里不能写成obj.s[j]
             if (!obj[s[j]]) {
                 obj[s[j]] = 1;
@@ -48,3 +75,5 @@ var lengthOfLongestSubstring = function(s) {
 };
 
 console.log(lengthOfLongestSubstring("abcabcabcbde"));
+
+
